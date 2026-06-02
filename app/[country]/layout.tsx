a7ff2efe073697326fd3c929edac3e404bec5bb6
khush prajapati <khush.prajapati@tinyscript.in>
2026-06-02 09:49:52 +0530

    layout error resolve

--- app/[country]/layout.tsx
+++ app/[country]/layout.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname, useParams } from "next/navigation";
 import { Inter } from "next/font/google";
+// @ts-expect-error Missing type declaration for global CSS import
 import "../globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
